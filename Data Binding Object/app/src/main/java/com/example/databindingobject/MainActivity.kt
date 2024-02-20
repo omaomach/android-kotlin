0d26b6443cd44b4c3941e7a8d0da23f2608c2bd3
omaomach <aaronjoash44@gmail.com>
2024-02-20 13:43:45 +0300

    databinding in the xml file

--- Data Binding Object/app/src/main/java/com/example/databindingobject/MainActivity.kt
+++ Data Binding Object/app/src/main/java/com/example/databindingobject/MainActivity.kt
@@ -11,16 +11,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         var user1 : User = User(1, "Joel", "Asiago")
-
-        binding.apply {
-            userId.text = user1.id.toString()
-            username.text = user1.name
-            tvPassword.text = user1.password
-        }
+        binding.user = user1
 
     }
 }
