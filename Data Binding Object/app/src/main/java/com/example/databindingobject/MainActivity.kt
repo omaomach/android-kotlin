45e8b0a7fba9878279696f1e9cdf35463c93438f
omaomach <aaronjoash44@gmail.com>
2024-02-20 13:31:40 +0300

    worked on data binding with object in the MainActivity

--- Data Binding Object/app/src/main/java/com/example/databindingobject/MainActivity.kt
+++ Data Binding Object/app/src/main/java/com/example/databindingobject/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.databindingobject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingobject.data.User
+import com.example.databindingobject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var user1 : User = User(1, "Joel", "Asiago")
+
+        binding.apply {
+            userId.text = user1.id.toString()
+            username.text = user1.name
+            tvPassword.text = user1.password
+        }
+
     }
 }
---
+++ Data Binding Object/app/src/main/java/com/example/databindingobject/data/User.kt
@@ -0,0 +1,7 @@
+package com.example.databindingobject.data
+
+data class User(
+    var id: Int,
+    var name: String,
+    var password: String
+)
