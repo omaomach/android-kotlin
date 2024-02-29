cf42af8c7ab4f1881fa7c1e1c5bde763962bea69
omaomach <aaronjoash44@gmail.com>
2024-02-29 13:01:56 +0300

    worked on connecting viewmodel with repository

--- Contacts Manager App/app/src/main/java/com/example/contactsmanagerapp/ViewModel/UserViewModel.kt
+++ Contacts Manager App/app/src/main/java/com/example/contactsmanagerapp/ViewModel/UserViewModel.kt
@@ -4,8 +4,10 @@ import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.contactsmanagerapp.room.User
 import com.example.contactsmanagerapp.room.UserRepository
+import kotlinx.coroutines.launch
 
 class UserViewModel(private val repository: UserRepository) : ViewModel(), Observable {
 
@@ -14,7 +16,57 @@ class UserViewModel(private val repository: UserRepository) : ViewModel(), Obser
     private lateinit var userToUpdateOrDelete : User
 
     @Bindable
-    val inputName = MutableLiveData<String>()
-    val inputEmail = MutableLiveData<String>()
+    val inputName = MutableLiveData<String?>()
+    val inputEmail = MutableLiveData<String?>()
+
+    @Bindable
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+
+    @Bindable
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
+    }
+
+    fun saveOrUpdate() {
+        val name = inputName.value!! // the !! are called not null assertion operator, it will prevent insertion of null values in the database
+        val email = inputEmail.value!!
+
+        insert(User(0, name, email))
+
+        inputName.value = null
+        inputEmail.value = null
+
+    }
+
+    fun clearAllOrDelete() {
+        clearAll()
+    }
+
+    fun insert(user: User) = viewModelScope.launch {
+        repository.insert(user)
+    }
+
+    fun clearAll() = viewModelScope.launch {
+        repository.deleteAll()
+    }
+
+    fun update(user: User) = viewModelScope.launch {
+        repository.update(user)
+    }
+
+    fun delete(user: User) = viewModelScope.launch {
+        repository.delete(user)
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        TODO("Not yet implemented")
+    }
 
 }
