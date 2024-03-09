245c1e30d633dd96e0bd31e0068a1ef9e1c72961
omaomach <aaronjoash44@gmail.com>
2024-03-09 23:15:48 +0300

    added logic to MainActivity

--- My Notes/app/src/main/java/com/example/mynotes/MainActivity.kt
+++ My Notes/app/src/main/java/com/example/mynotes/MainActivity.kt
@@ -2,14 +2,38 @@ package com.example.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.mynotes.database.NoteDatabase
+import com.example.mynotes.databinding.ActivityMainBinding
+import com.example.mynotes.repository.NoteRepository
 import com.example.mynotes.viewmodel.NoteViewModel
+import com.example.mynotes.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
 
     lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewModel()
+
+    }
+
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+
+        noteViewModel = ViewModelProvider(
+            this,
+            viewModelProviderFactory).get(NoteViewModel::class.java)
+
     }
 }
